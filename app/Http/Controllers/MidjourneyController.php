2a583b47b35b87589dfd6dff7d05a9662995226c
Victor Tang <tanghengzhi@live.com>
2023-11-24 10:23:05 +0800

    first commit

---
+++ app/Http/Controllers/MidjourneyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MidjourneyTask;
+use Ferranfg\MidjourneyPhp\Midjourney;
+use Ferranfg\MidjourneyPhp\Prompts;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+
+/**
+ * @group Ai Creator
+ */
+class MidjourneyController extends Controller
+{
+    /**
+     * Generate Images (Midjourney)
+     *
+     * @response 200
+     */
+    public function generate(Request $request)
+    {
+        $lock = Cache::lock('midjourney:generate', 60);
+
+        if (!$lock->get()) {
+            throw new TooManyRequestsHttpException(60);
+        }
+
+        $midjourneyTask = new MidjourneyTask();
+        $midjourneyTask->save();
+
+        $request->validate([
+            'imagePrompts' => 'string',
+            'textPrompt' => 'string',
+            'parameters' => 'string',
+            'discordChannelId' => 'string',
+            'discordUserToken' => 'string',
+        ]);
+
+        $prompts = new Prompts(
+            $request->imagePrompts,
+            $request->textPrompts,
+            $request->paramters,
+        );
+        $midjourney = new Midjourney($request->discordChannelId, $request->discordUserToken);
+
+        // It takes about 1 minute to generate and upscale an image
+        return $midjourney->generate($prompts);
+    }
+}
